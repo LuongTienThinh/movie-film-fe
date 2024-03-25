73eba6a5f6af1741b7ec185930d43c53b35a8c1d
LuongTienThinh <ltthinh2001120422@gmail.com>
2024-03-25 22:09:57 +0700

    feat: code banner (no responsive)

--- app/src/assets/icons/dark/index.tsx
+++ app/src/assets/icons/dark/index.tsx
@@ -68,6 +68,16 @@ const Youtube = ({ ...props }) => <svg width="41" height="40" viewBox="0 0 41 40
   <path d="M29 6.6665H12.3333C7.33333 6.6665 4 9.99984 4 14.9998V24.9998C4 29.9998 7.33333 33.3332 12.3333 33.3332H29C34 33.3332 37.3333 29.9998 37.3333 24.9998V14.9998C37.3333 9.99984 34 6.6665 29 6.6665ZM23.8167 21.7165L19.7 24.1832C18.0333 25.1832 16.6666 24.4165 16.6666 22.4665V17.5165C16.6666 15.5665 18.0333 14.7999 19.7 15.7999L23.8167 18.2665C25.4 19.2332 25.4 20.7665 23.8167 21.7165Z" fill="#F9FAFB" />
 </svg>
 
+const BtnPrev = ({ ...props }) => <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+  <path d="M14.9999 36.6667H24.9999C33.3333 36.6667 36.6666 33.3333 36.6666 25V15C36.6666 6.66668 33.3333 3.33334 24.9999 3.33334H14.9999C6.66659 3.33334 3.33325 6.66668 3.33325 15V25C3.33325 33.3333 6.66659 36.6667 14.9999 36.6667Z" stroke="#F9FAFB" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+  <path d="M22.1001 25.8833L16.2334 20L22.1001 14.1167" stroke="#F9FAFB" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+</svg>
+
+const BtnNext = ({ ...props }) => <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+  <path d="M14.9999 36.6667H24.9999C33.3333 36.6667 36.6666 33.3333 36.6666 25V15C36.6666 6.66668 33.3333 3.33334 24.9999 3.33334H14.9999C6.66659 3.33334 3.33325 6.66668 3.33325 15V25C3.33325 33.3333 6.66659 36.6667 14.9999 36.6667Z" stroke="#F9FAFB" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+  <path d="M17.8999 25.8833L23.7666 20L17.8999 14.1167" stroke="#F9FAFB" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+</svg>
+
 
 const DarkIcons = ({ iconName, ...props }: IThemeIcon) => {
   switch (iconName) {
@@ -106,6 +116,12 @@ const DarkIcons = ({ iconName, ...props }: IThemeIcon) => {
 
     case 'direct-notification':
       return <DirectNotification {...props} />;
+
+    case 'btn-prev':
+      return <BtnPrev {...props} />;
+
+    case 'btn-next':
+      return <BtnNext {...props} />;
 
     default:
       return null;
