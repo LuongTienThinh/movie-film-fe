a945064d7d5b8b20e3734531f59445c1022c41cd
Tylor Luong <tylor@digityze.asia>
2024-12-17 21:28:50 +0700

    update line-height and height film component

--- app/src/components/film/index.tsx
+++ app/src/components/film/index.tsx
@@ -7,7 +7,7 @@ const Film = ({ name, slug, poster_url, className, id, ...props }: IFilm) => {
   return (
     <Link to={`/film-detail/${id}/${slug}`} className={className}>
       <img className='pointer-events-none mb-p2 h-[224px] w-full rounded-p2 sm:h-[238px]' src={poster_url} alt='' />
-      <div className='line-clamp-2 h-[40px] text-lg font-extrabold leading-5' title={name}>
+      <div className='line-clamp-2 h-12 text-lg font-extrabold leading-6' title={name}>
         {name}
       </div>
     </Link>
